06042033665cae1a9f285d642591b1a7281d8b4e
Isaev Nikita <nikitavegas95@gmail.com>
2025-04-19 10:52:05 +0300

    feat: валидация email

--- js/getInputValue.js
+++ js/getInputValue.js
@@ -3,12 +3,18 @@ const button = document.getElementById('alertButton');
 
 let inputValue = '';
 
+const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]{3,}@[^\s@]{2,}\.[^\s@]{2,}$/;
+    return emailRegex.test(email);
+}
+
 emailInput.addEventListener('input', (event) => {
     inputValue = event.target.value;
 })
 
 button.addEventListener('click', () => {
     if (!inputValue) return alert('Вы ничего не ввели')
+    if (!validateEmail(inputValue)) return alert('Введите валидный email')
     alert(`Спасибо за подписку! 🎉 ${inputValue}`);
 });
 
