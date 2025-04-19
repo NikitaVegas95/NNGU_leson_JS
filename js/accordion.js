c9625348bed62056018e4a3fbb65f9389cad1e28
Isaev Nikita <nikitavegas95@gmail.com>
2025-04-19 11:31:23 +0300

    feat: добавили поворот svg при раскрытии accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,14 +1,20 @@
 document.querySelectorAll('.accordion').forEach((el) => {
     el.addEventListener('click', () => {
-
         let content = el.nextElementSibling;
+        // Находим SVG внутри текущего аккордеона
+        let svg = el.querySelector('#svg');
+
         console.log(content);
 
         if (content.style.maxHeight) {
-            document.querySelectorAll('.content-question').forEach((el) => el.style.maxHeight = null)
+            document.querySelectorAll('.content-question').forEach((el) => el.style.maxHeight = null);
+            if (svg) svg.classList.remove('svg');
         } else {
-            document.querySelectorAll('.content-question').forEach((el) => el.style.maxHeight = null)
+            document.querySelectorAll('.content-question').forEach((el) => el.style.maxHeight = null);
+            // Удаляем класс у всех SVG
+            document.querySelectorAll('#svg').forEach((s) => s.classList.remove('svg'));
             content.style.maxHeight = content.scrollHeight + 'px';
+            if (svg) svg.classList.add('svg');
         }
     })
-})
+});
